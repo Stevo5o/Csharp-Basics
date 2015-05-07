9e8bca6dc891ac5ebf057924ec1cd6e3b95fc5ed
Stevo5o <stevo.joc@gmail.com>
2015-05-07 15:00:04 +0100

    Working with vars, expressions, statements & operators

---
+++ Lesson04/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string myStringVariable;
+            //string myInitializedStringVariable = "SteJ";
+
+            //int myInterger = 4;
+            //double mydouble = 3.14;
+            //bool myBoolean = true;
+
+            //string myvalue;
+            //string myValue;
+
+            /* string hello; // hello is the bucket
+            hello = "hello world"; // string literal goes into the hello bucket
+            MessageBox.Show(hello); // hello var
+            MessageBox.Show("hello"); // string literal */
+
+            //// declare two vars
+            //string firstTextBox = textBox1.Text;
+            //string secondTextBox = textBox2.Text;
+
+            //label1.Text = firstTextBox + " " + secondTextBox; // statement
+
+            //int myValue = 3; // data type interger
+            //double myOtherValue; // data type double
+            //myOtherValue = myValue; // VS converts from int to double
+
+            //int myValue; // data type interger
+            //double myOtherValue = 3.14; // data type double
+            //myValue = myOtherValue; // VS converts from int to double
+
+            //int firstTextBox = 0;
+            //int secondTextBox = 0;
+            //int result = 0;
+
+            //firstTextBox = int.Parse(textBox1.Text);
+            //secondTextBox = int.Parse(textBox2.Text);
+
+            //result = firstTextBox + secondTextBox;
+            //label1.Text = result.ToString();
+        }
+    }
+}
